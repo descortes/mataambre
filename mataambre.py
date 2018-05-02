04520383ca16f9345cff54dc0a181854abd94705
descortes <o.cortesmauro@gmail.com>
2018-05-01 21:24:03 -0300

    refactor de proyecto general

--- mataambre.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import Response
-
-mataambre = Flask(__name__)
-
-@mataambre.route('/cortesdecarne/mataambre', methods = ['GET'])
-def getmataambre():
-	return 'mataambre \n'
